c8fa6c28d3c105c66ea83ac10352cb44637707f4
henrycollin <33245560+henrycollin@users.noreply.github.com>
2017-11-28 18:07:13 +0800

    20171128

--- modeller_test.py
+++ modeller_test.py
@@ -7,11 +7,16 @@ Created on Mon Nov 27 12:34:05 2017
 
 ##### clustal omega inout file #####
 
-import os
+import os, re
 #file = os.path.abspath("C:\\Users\\bio608\\Documents\\GitHub\\xhr\\template_and_target_file\\1KLU.txt")
 with open("C:\\Users\\bio608\\Documents\\GitHub\\xhr\\template_and_target_file\\1KLU.pir", 'rt') as f:
     seq = list()
+    line3 = str()
     for line in f:
-        print (line)
-    
+        line2 = line.split('\n');
+        seq.append(line2)
+        ### list to string ###
+        #line3 = ''.join(str(e) for e in seq);
+
+print (seq[0],seq[1])
     
